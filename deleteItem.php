ab76821123fa3e55168712e8ea7e41f955c22be5
Frangilio <farasy.frangilio@gmail.com>
2023-11-01 22:19:06 +0300

    Modification du fichier deleteItem

--- deleteItem.php
+++ deleteItem.php
@@ -2,12 +2,14 @@
 include('fct/item.php');
 include('fct/request.php');
 include('config/app.php');
+include('config/db.php');
+include('connexion.php');
 
-$items = getItems();
 $id = get('item');
 
-unset($items[$id]);
-
-saveItems($items);
+$query = 'DELETE FROM todo WHERE id=:id';
+$stmt = $pdo->prepare($query);
+$stmt->bindParam('id', $id);
+$stmt->execute();
 
 header('Location:index.php');
